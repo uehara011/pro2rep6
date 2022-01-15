72f903a91d4ed1c24f9eb83b607fdc8606b6a0a3
Uehara YUTO <e215747@ie.u-ryukyu.ac.jp>
2022-01-16 00:53:45 +0900

    4th commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215747/hand.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e215747;
+
+public class hand {
+    public void playerhand(String playerhand){
+        System.out.println("あなたの手は「" + playerhand + "」です。");
+    }
+
+    
+    public void enemyOutPut(String enemyhand){
+        System.out.println("相手は「" + enemyhand + "。");
+    }
+
+   
+    public void ruleExplanation(){
+        System.out.printf("「グー」or「チョキ」or「パー」or「終了」を出せるよ!\n「終了」を入力するとジャンケンが終了するよ!\n");
+    }
+
+    
+}
+    
+
