62719bca341f1b4c0f50ef6d35f576eb466a57b4
Uehara YUTO <e215747@ie.u-ryukyu.ac.jp>
2022-01-16 00:44:42 +0900

    3rd commit

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import jp.ac.uryukyu.ie.e215747.janken;
+
+public class Main {
+    public static void main(String[] args){
+            janken jan = new janken();
+            jan.janken();
+        }
+    
+
+    }
+
