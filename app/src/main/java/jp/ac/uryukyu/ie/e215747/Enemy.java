fd1f614440ea71b2ecbbf49b150b4c5f2335d12f
Uehara YUTO <e215747@ie.u-ryukyu.ac.jp>
2022-01-16 01:10:08 +0900

    5th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215747/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215747/Enemy.java
@@ -2,8 +2,8 @@ package jp.ac.uryukyu.ie.e215747;
 import java.util.Random;
 
 public class Enemy {
-    public String enemyInPut(){
-        String[] hand = { "グー", "チョキ", "パー" };
+    public String enemyH(){
+        String[] hand = { "グー", "チョキ", "パー" };                                                                                                                                                                                                                                                              
         Random r = new Random();
         String enemyhand = hand[r.nextInt(3)];
         return enemyhand;
